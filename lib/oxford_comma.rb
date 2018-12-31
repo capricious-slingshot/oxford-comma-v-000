392fd766cd4edff459e1a388e44f80c968b5a746
Carden Z <ephemere@protonmail.com>
2018-12-31 03:16:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,11 @@
 def oxford_comma(array)
+	return array.join if array.length == 1
 	x = array.length - 1
+	return array.join if array.length == 1
   array.each_with_index do |word, index|
 		if index == x
       word[index] = "and " + word[index]
+		elsif index[]
 		else
 			word[index] + ", "
 		end
